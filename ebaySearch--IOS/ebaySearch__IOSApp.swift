78f705933b30f4c8d55b295efe61f139fd5311d5
rickmli <rmli@usc.edu>
2023-12-03 12:25:58 -0800

    Initial Commit

---
+++ ebaySearch--IOS/ebaySearch__IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebaySearch__IOSApp.swift
+//  ebaySearch--IOS
+//
+//  Created by Rick Li on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ebaySearch__IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
